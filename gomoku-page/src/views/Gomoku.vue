ff16b1dfc9b4ec0c8ed544ba561b8b1de2a1a155
maeshiro <maeshiro@mfac.jp>
2019-05-15 13:25:03 +0900

    vue cli3に移行

---
+++ gomoku-page/src/views/Gomoku.vue
@@ -0,0 +1,14 @@
+<template>
+  <Gomoku></Gomoku>
+</template>
+
+<script>
+import { Gomoku } from "@/components/Gomoku";
+
+export default {
+  name: "gomoku",
+  components: {
+    Gomoku
+  }
+};
+</script>
